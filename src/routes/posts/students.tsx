63b72e653cc146da1bd5f80af5fa3590824986b3
LizPillajo <ldpillajo@uce.edu.ec>
2025-12-01 11:49:35 -0500

    Creation of “students” route to connect to sample API.

---
+++ src/routes/posts/students.tsx
@@ -0,0 +1,36 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/posts/students')({
+  loader: async () => {
+
+    const response = await fetch(
+      `https://wprogramming-diagnostic-test.onrender.com/student/1`
+    )
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch post')
+    }
+
+    return response.json()
+  },
+
+  component: StudentsComponent,
+})
+
+function StudentsComponent() {
+  const student = Route.useLoaderData() as {
+    id: number
+    name: string
+    lastname: string
+    age: number
+  }
+
+  return (
+    <div>
+      <h1>Estudiante #{student.id}</h1>
+      <p><strong>Nombre:</strong> {student.name}</p>
+      <p><strong>Lastname:</strong> {student.lastname}</p>
+      <p><strong>Age:</strong> {student.age}</p>
+    </div>
+  )
+}
