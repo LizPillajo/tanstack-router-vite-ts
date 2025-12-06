e43f4913ffa7bba743c7ab642c70b5a42a33b4f2
LizPillajo <ldpillajo@uce.edu.ec>
2025-12-05 23:02:51 -0500

    edit example for presentation

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,18 +1,17 @@
-
-import {Link, Outlet, createRootRoute } from '@tanstack/react-router' 
+import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
 
 export const Route = createRootRoute({
   component: RootComponent,
-})
+});
 
 function RootComponent() {
   return (
     <>
       <div>
-        <Link to= "/"> Home</Link>
-        <Link to= "/about"> About</Link>
+        <Link to="/"> Home</Link>
+        <Link to="/about"> About</Link>
       </div>
       <Outlet />
     </>
-  )
+  );
 }
